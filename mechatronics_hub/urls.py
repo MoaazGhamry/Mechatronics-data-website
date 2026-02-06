c5fbce608c20000a74f38d48f3791cd764570927
Moaaz Elghamry <29212023100207@meceng.znu.edu.eg>
2026-02-06 20:50:30 +0200

    Fix PWA paths: Serve manifest and sw.js from root

--- mechatronics_hub/urls.py
+++ mechatronics_hub/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
+urlpatterns += [
+    path('manifest.json', serve, {'document_root': settings.STATIC_ROOT, 'path': 'manifest.json'}),
+    path('sw.js', serve, {'document_root': settings.STATIC_ROOT, 'path': 'sw.js'}),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
